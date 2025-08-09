b32d4962ccf9c7a69fcc9289dc27121064b28071
AnthonyJosueS <115773852+AnthonyJosueS@users.noreply.github.com>
2025-08-09 00:53:33 -0500

    database config

--- Backend/src/lib/prisma.ts
+++ Backend/src/lib/prisma.ts
@@ -2,14 +2,14 @@ import { PrismaMssql } from "@prisma/adapter-mssql";
 import { PrismaClient } from "@prisma/client";
 
 const config = {
-    server: 'localhost',
-    port: 58630,
-    database: 'holidays_calendar_db',
-    user: 'sa',
-    password: 'BD1998',
+    server: process.env.DB_SERVER,
+    port: process.env.DB_PORT,
+    database: process.env.DB_DATABASE,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
     options: {
-        encrypt: true, 
-        trustServerCertificate: true,
+        encrypt: process.env.DB_ENCRYPT || true, 
+        trustServerCertificate: process.env.DB_TRUST_SERVER_CERTIFICATE || true,
     },
 }
 
